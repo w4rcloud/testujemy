f1ab58f217288b69138dbc6260df2537e0372a74
Mike <you@example.com>
2020-07-11 21:34:17 +0200

    Excercises continued

--- src/main/java/java_exercises_1_sda/Zad8.java
+++ src/main/java/java_exercises_1_sda/Zad8.java
@@ -5,7 +5,20 @@ package java_exercises_1_sda;
 public class Zad8 {
 
     public static void checkIfArithmeticProgression (int[] numbers){
-        int difference;
-        
+        int difference = numbers[0] - numbers[1];
+
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] - numbers[i+1] == difference){
+                System.out.println(true);
+        } else {
+                System.out.println(false);
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 3, 5};
+        checkIfArithmeticProgression(numbers);
     }
 }
