287c72559f2a903432b13d87c59fad0edb4d8eb4
Mike <you@example.com>
2020-07-19 13:16:22 +0200

    Casting/rzutowanie

--- src/main/java/java_exercises_1_mz/oop/Main2.java
+++ src/main/java/java_exercises_1_mz/oop/Main2.java
@@ -1,5 +1,8 @@
 package java_exercises_1_mz.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main2 {
 
     public static void main(String[] args) {
@@ -8,5 +11,20 @@ public class Main2 {
         SportCar bmw = new SportCar("BMW", "X7", "black", v8, 7);
 
         System.out.println(bmw.toString());
+        Car porsche = new SportCar("Porsche", "Panamera", "red", v8, 4);
+        //upwards casting - rzutowanie w gore
+
+//        jezeli SportCar rzutujemy na Car - rzutowanie w gore, obcina czesc metod
+//        jezeli Car -> SportCar - rzutowanie w dol, dostaje dostep do metod zdefiniowanych tylko w SportCar
+
+        SportCar sportsCar = (SportCar) porsche;
+
+        List<SportCar> sportCarsList = new ArrayList<>();
+        sportCarsList.add(bmw);
+        sportCarsList.add((SportCar) porsche);
+
+        for (SportCar car : sportCarsList) {
+            car.getEngine();
+        }
     }
 }
